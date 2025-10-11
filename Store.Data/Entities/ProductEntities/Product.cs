da3540f77d833b0c2881410b9cbe5fbb60c59179
Guirguis <ephraimhedia@gmail.com>
2025-10-11 14:00:49 +0300

    updateTheRelationshipBetweenColor_SizeAndProduct

--- Store.Data/Entities/ProductEntities/Product.cs
+++ Store.Data/Entities/ProductEntities/Product.cs
@@ -10,12 +10,8 @@ namespace Store.Data.Entities.ProductEntities
         public int CategoryId { get; set; }
         public Subcategory? Subcategory { get; set; }
         public int? SubcategoryId { get; set; }
-        public List<ProductColor> ProductColors { get; set; } = new List<ProductColor>();
-        public List<ProductSize> ProductSizes { get; set; } = new List<ProductSize>();
         public List<ProductColorJoin> ProductColorJoins { get; set; } = new();
         public List<ProductSizeJoin> ProductSizeJoins { get; set; } = new();
-
-
         public List<ProductStock> ProductStocks { get; set; } = new List<ProductStock>();
         public List<Image> Images { get; set; } = new List<Image>();
         public Discount? Discount { get; set; }
--- Store.Services/Services/ProductService/Dtos/ProductProfile.cs
+++ Store.Services/Services/ProductService/Dtos/ProductProfile.cs
@@ -14,14 +14,10 @@ namespace Store.Services.Services.ProductService.Dtos
                     .ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => src.Category.Name))
                     .ForMember(dest => dest.SubcategoryName, opt => opt.MapFrom(src => src.Subcategory != null ? src.Subcategory.Name : null))
                     .ForMember(dest => dest.DiscountPercentage, opt => opt.MapFrom(src => src.Discount != null ? (decimal?)src.Discount.Percentage : null))
-                    .ForMember(dest => dest.ProductColors, opt => opt.MapFrom(src => src.ProductColors.Select(c => c.ColorName).ToList()))
-                    .ForMember(dest => dest.ProductSizes, opt => opt.MapFrom(src => src.ProductSizes.Select(s => s.Name).ToList()))
                     .ForMember(dest => dest.Images, opt => opt.MapFrom(src => src.Images.Select(i => i.Url).ToList()));
 
                 // Map ProductCreateDto → Product
                 CreateMap<ProductCreateDto, Product>()
-                    .ForMember(dest => dest.ProductColors, opt => opt.Ignore())
-                    .ForMember(dest => dest.ProductSizes, opt => opt.Ignore())
                     .ForMember(dest => dest.Images, opt => opt.Ignore());
 
         }
